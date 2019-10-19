qaisjp/inf-ttds

commit 986847b900ebad3eae5fa2dc75a19aa23dde6ec8

lab3: improve error handling for empty searches

lab3/doc.py
@@ -77,6 +77,9 @@ def search(docmap, index, query, use_tfidf):
 
                 docs = nearby_docs
                 found = True
+            else:
+                print("Terms '{}' and '{}' not found".format(term_a, term_b))
+                continue
         elif qpart.text in index:
             docs = index[qpart.text].keys()
             found = True
@@ -105,7 +108,7 @@ def search(docmap, index, query, use_tfidf):
 
     if op == "AND":
         inclusions = map(set, inclusions)
-        inclusions = reduce(set.intersection, inclusions)
+        inclusions = reduce(set.intersection, inclusions, set())
     else:
         inclusions = itertools.chain.from_iterable(inclusions)
 

commit 27578c6c415eccfcc8e6219cd3c7bbf8681c4bea

lab3: partially revert 986847b900ebad3eae5fa2dc75a19aa23dde6ec8
caused empty stuffs for ANDs

lab3/doc.py
@@ -119,7 +119,7 @@ def search(docmap, index, query, use_tfidf):
 
     if op == "AND":
         inclusions = map(set, inclusions)
-        inclusions = reduce(set.intersection, inclusions, set())
+        inclusions = reduce(set.intersection, inclusions)
     else:
         inclusions = itertools.chain.from_iterable(inclusions)
 